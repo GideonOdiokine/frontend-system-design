edb57d8b9d4800789e660f7cd3d52fb919eb640e
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-06-13 01:00:37 +0100

    setup relationship among author  and books

--- Networking/GraphQL/resolver.js
+++ Networking/GraphQL/resolver.js
@@ -1,26 +1,41 @@
+const data = {
+  authors: [
+    { id: "1", name: "Gideon Odiokine", bookIds: ["101", "102"] },
+    { id: "2", name: "Akshay Saini", bookIds: ["103"] },
+  ],
+  books: [
+    {
+      id: "101",
+      title: "Namaste Frontend System Design",
+      publishedYear: 2000,
+      authorId: "1",
+    },
+    { id: "102", title: "Book 2", publishedYear: 2010, authorId: "1" },
+    { id: "103", title: "Book 3", publishedYear: 2020, authorId: "2" },
+  ],
+};
+
 export const resolvers = {
+  Book: {
+    author: (parent, args, context, info) => {
+      return data.authors.find(
+        (authorDetail) => authorDetail.id === parent.authorId
+      );
+    },
+  },
+  Author: {
+    books: (parent, args, context, info) => {
+      return data.books.filter((bookInfo) =>
+        parent.bookIds.includes(bookInfo.id)
+      );
+    },
+  },
   Query: {
     authors: () => {
-      return [
-        {
-          id: 1,
-          name: "Gideon Odiokine",
-        },
-      ];
+      return data.authors;
     },
     books: () => {
-      return [
-        {
-          id: 1,
-          title: "Okon Bassey Odiokine",
-          publishedYear: 2023,
-        },
-        {
-          id: 2,
-          title: "Gideon Bassey Odiokine",
-          publishedYear: 2023,
-        },
-      ];
+      return data.books;
     },
   },
 };
