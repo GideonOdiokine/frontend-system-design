8dba0131d833770576b732372c8ade3fe4e86788
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-06-29 04:14:46 +0100

    normalized data

---
+++ database&caching/normalization/normalize.js
@@ -0,0 +1,25 @@
+// Normalized Data
+
+const state = {
+  users: {
+    byIds: {
+      1: {
+        id: 1,
+        name: "Alice",
+      },
+      2: {
+        id: 2,
+        name: "Bob",
+      },
+    },
+  },
+  posts: {
+    byIds: {
+      101: { id: 101, title: "Post 1", userId: 1 },
+      102: { id: 102, title: "Post 2", userId: 2 },
+      103: { id: 103, title: "Post 3", userId: 1 },
+    },
+  },
+};
+
+
